439c04b02e2cc4fc81600a85c0f53ccaea43610c
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-15 02:13:50 +0200

    feat: add the constructor in the class Movie

--- index.php
+++ index.php
@@ -7,10 +7,19 @@ class Movie {
     public $streaming;
     public $averageVote;
     public $genre;
-};
 
-$movie1 = new Movie();
-$movie2 = new Movie();
+    function __construct($_name, $_year, $_duration, $_streaming, $_averageVote, $_genre) {
+        $this->name = $_name;
+        $this->year = $_year;
+        $this->duration = $_duration;
+        $this->streaming = $_streaming;
+        $this->averageVote = $_averageVote;
+        $this->genre = $_genre;
+    }
+}
+
+$movie1 = new Movie('Love Actually', 2003, '2h 15m', 'Netflix', 7.6, 'commedia');
+$movie2 = new Movie('Harry, ti presento Sally', 1989, '1h 36m', 'Disney+', 7.7, 'romance');
 
 var_dump($movie1);
 var_dump($movie2);
@@ -26,6 +35,5 @@ var_dump($movie2);
     <title>OOP</title>
 </head>
 <body>
-    <h2>ciao</h2>
 </body>
 </html>
