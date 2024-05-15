ccd45e29e416264122c9fca3acb34a4bdbe5e825
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-15 02:41:30 +0200

    feat: add the movies in the page using a foreach and used the functions to show the informations

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ class Movie {
     public $averageVote;
     public $genre;
 
-    function __construct($_name, $_year, $_duration, $_streaming, $_averageVote, $_genre) {
+    public function __construct($_name, $_year, $_duration, $_streaming, $_averageVote, $_genre) {
         $this->name = $_name;
         $this->year = $_year;
         $this->duration = $_duration;
@@ -16,14 +16,25 @@ class Movie {
         $this->averageVote = $_averageVote;
         $this->genre = $_genre;
     }
+
+    public function getTitleMovie() {
+        return $this->name;
+    }
+
+    public function getInfoMovie() {
+        return $this->year . ' - ' . $this->duration . ' - ' . $this->streaming;
+    }
 }
 
 $movie1 = new Movie('Love Actually', 2003, '2h 15m', 'Netflix', 7.6, 'commedia');
 $movie2 = new Movie('Harry, ti presento Sally', 1989, '1h 36m', 'Disney+', 7.7, 'romance');
 
-var_dump($movie1);
-var_dump($movie2);
 
+
+$movies = [
+    $movie1,
+    $movie2
+]
 
 ?>
 
@@ -32,8 +43,18 @@ var_dump($movie2);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
     <title>OOP</title>
 </head>
 <body>
+    <div class="container">
+        <?php foreach($movies as $movie) { ?>
+        <div class="movie-container">
+            <h2><?php echo $movie->getTitleMovie(); ?></h2>
+            <p><?php echo $movie->getInfoMovie(); ?></p>
+        </div>
+        <?php }?>
+    </div>
+    
 </body>
 </html>
